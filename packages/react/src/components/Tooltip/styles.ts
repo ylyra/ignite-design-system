f733ab4bfea02a7632654ced345c707b76c65945
Yan Lyra <yanlyra3@gmail.com>
2022-10-15 22:36:45 -0300

    feat(tooltip): added animation when hover trigger

--- packages/react/src/components/Tooltip/styles.ts
+++ packages/react/src/components/Tooltip/styles.ts
@@ -1,5 +1,25 @@
 import * as RadixTooltip from '@radix-ui/react-tooltip';
-import { styled } from '../../styles';
+import { keyframes, styled } from '../../styles';
+
+const slideUpAndFade = keyframes({
+  '0%': { opacity: 0, transform: 'translateY(2px)' },
+  '100%': { opacity: 1, transform: 'translateY(0)' },
+});
+
+const slideRightAndFade = keyframes({
+  '0%': { opacity: 0, transform: 'translateX(-2px)' },
+  '100%': { opacity: 1, transform: 'translateX(0)' },
+});
+
+const slideDownAndFade = keyframes({
+  '0%': { opacity: 0, transform: 'translateY(-2px)' },
+  '100%': { opacity: 1, transform: 'translateY(0)' },
+});
+
+const slideLeftAndFade = keyframes({
+  '0%': { opacity: 0, transform: 'translateX(2px)' },
+  '100%': { opacity: 1, transform: 'translateX(0)' },
+});
 
 export const Root = styled(RadixTooltip.Root, {});
 export const Trigger = styled(RadixTooltip.Trigger, {
@@ -11,6 +31,18 @@ export const Content = styled(RadixTooltip.Content, {
   background: '$gray900',
   borderRadius: '$xs',
   filter: 'drop-shadow(4px 16px 24px rgba(0, 0, 0, 0.25))',
+
+  '@media (prefers-reduced-motion: no-preference)': {
+    animationDuration: '400ms',
+    animationTimingFunction: 'cubic-bezier(0.16, 1, 0.3, 1)',
+    willChange: 'transform, opacity',
+    '&[data-state="delayed-open"]': {
+      '&[data-side="top"]': { animationName: slideDownAndFade },
+      '&[data-side="right"]': { animationName: slideLeftAndFade },
+      '&[data-side="bottom"]': { animationName: slideUpAndFade },
+      '&[data-side="left"]': { animationName: slideRightAndFade },
+    },
+  },
 });
 export const Arrow = styled(RadixTooltip.Arrow, {
   fill: '$gray900',
